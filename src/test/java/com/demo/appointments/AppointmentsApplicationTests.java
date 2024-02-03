6c2c20443f07310ff552681d493273478802ac58
Artem Aldungarov <aldungarov.ar@gmail.com>
2024-02-03 18:48:35 +0400

    Initial project structure and dependencies

---
+++ src/test/java/com/demo/appointments/AppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
